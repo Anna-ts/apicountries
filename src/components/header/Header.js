4162130d4b7bdabdf9a40c100145837a5dce9667
Anna <anna.tseliuba@gmail.com>
2022-06-30 06:59:24 -0700

    layout change

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,14 @@
 import * as React from "react"
-import {header, darkModeIcon, logo, mode} from './header.module.css'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faMoon} from '@fortawesome/free-regular-svg-icons'
-import {faMoon as faMoonSolid} from '@fortawesome/free-solid-svg-icons'
+import {header, logo} from './header.module.css'
 
 
-const Header = ({theme, switchTheme}) => {
+
+const Header = () => {
 
     return (
         <header className={header}>
             <div className={logo}>
                 <span>Where in the world?</span>
-            </div>
-            <div className={mode} onClick={switchTheme}>
-                {theme === 'light'
-                    ? <><FontAwesomeIcon className={darkModeIcon} icon={faMoon} size="1x"/> <span>Dark Mode</span></>
-                    : <><FontAwesomeIcon className={darkModeIcon} icon={faMoonSolid} size="1x"/>
-                        <span>Dark Mode</span></>
-                }
             </div>
         </header>
     )
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,28 +1,12 @@
 import * as React from "react"
-import { useEffect, useState } from 'react';
-import useLocalStorage from 'use-local-storage'
 import Header from "../header/Header"
 import {container} from './layout.module.css'
 
 const Layout = ({children}) => {
-
-    const [defaultDark, setDefaultDark] = useState(false)
-
-    useEffect(() => {
-        setDefaultDark(window.matchMedia('(prefers-color-scheme: dark)').matches)
-    }, [])
-
-    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light')
-
-    const switchTheme = () => {
-        const newTheme = theme === 'light' ? 'dark' : 'light'
-        setTheme(newTheme)
-    }
-
     return (
-        <main className={container} data-theme={theme}>
+        <main className={container}>
             <title>pageTitle</title>
-            <Header theme={theme} switchTheme={switchTheme}/>
+            <Header/>
             {children}
         </main>
     )
